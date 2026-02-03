c81be802b820357e622f74fcd7f1aa6315b8e405
Rishank Kashyap <rishankkashyap03@example.com>
2026-02-03 23:59:27 +0530

    Feat: Added eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended", // Must be last to override other formatting rules
+  ],
+  rules: {
+    "prettier/prettier": "error", // Reads config from .prettierrc
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  ignorePatterns: ["dist", "node_modules"],
+};
